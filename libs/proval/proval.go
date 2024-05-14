17722b4a93ba14a041abafee94784e1e9d6a8cc0
irwinarruda <arruda.irwin@gmail.com>
2024-05-13 21:22:23 -0300

    feat: adds tests to Max and Email

--- libs/proval/proval.go
+++ libs/proval/proval.go
@@ -1,60 +1,5 @@
 package proval
 
-import (
-	"errors"
-	"regexp"
-)
-
-var emailRegex = regexp.MustCompile(`^([A-Z0-9_'+\-\.]+)([A-Z0-9_+\-]+)@([A-Z0-9][A-Z0-9\-]*\.)+[A-Z]{2,}$`)
-
-type StringValidator struct {
-	validations map[string]string
-	min         *int
-	max         *int
-}
-
-func (s *StringValidator) Min(min int, message string) *StringValidator {
-	s.min = &min
-	s.validations["min"] = message
-	return s
-}
-
-func (s *StringValidator) Max(max int, message string) *StringValidator {
-	s.max = &max
-	s.validations["max"] = message
-	return s
-}
-
-func (s *StringValidator) Email(message string) *StringValidator {
-	s.validations["email"] = message
-	return s
-}
-
-func (s *StringValidator) Validate(val any) []error {
-	errs := []error{}
-	parsedVal, ok := val.(string)
-	if !ok {
-		errs = append(errs, errors.New(s.validations["default"]))
-	}
-	for key, message := range s.validations {
-		if key == "min" {
-			if len(parsedVal) < *s.min {
-				errs = append(errs, errors.New(message))
-			}
-		} else if key == "max" {
-			if len(parsedVal) > *s.min {
-				errs = append(errs, errors.New(message))
-			}
-
-		} else if key == "email" {
-			if !emailRegex.MatchString(parsedVal) {
-				errs = append(errs, errors.New(message))
-			}
-		}
-	}
-	return errs
-}
-
 type Val struct{}
 
 func (v *Val) String(message string) *StringValidator {
@@ -63,6 +8,10 @@ func (v *Val) String(message string) *StringValidator {
 	}
 	validator.validations["default"] = message
 	return &validator
+}
+
+func (v *Val) Object(message string) {
+	// TODO:
 }
 
 func (v *Val) ToStringSlice(errs []error) []string {
--- tests/proval_test.go
+++ tests/proval_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/irwinarruda/pro-cris-server/libs/proval"
@@ -21,12 +22,12 @@ func TestValString(t *testing.T) {
 	errs := v.String(message).Validate(4)
 	ok := HasMessage(errs, message)
 	if !ok {
-		t.Fatal("It should return error if primitive value is passed")
+		t.Fatal("It should return error if primitive value is passed.")
 	}
 	errs = v.String(message).Validate(struct{}{})
 	ok = HasMessage(errs, message)
 	if !ok {
-		t.Fatal("It should return error if struct value is passed")
+		t.Fatal("It should return error if struct value is passed.")
 	}
 }
 
@@ -36,6 +37,55 @@ func TestValStringMin(t *testing.T) {
 	errs := v.String("").Min(4, message).Validate("any")
 	ok := HasMessage(errs, message)
 	if !ok {
-		t.Fatal("It should return an error if message length is less than the param")
+		t.Fatal("It should return an error if message length is less than the param.")
+	}
+	errs = v.String("").Min(3, message).Validate("any")
+	ok = HasMessage(errs, message)
+	if ok {
+		t.Fatal("It should not return an error if message length is greater than the param.")
+	}
+	errs = v.String("").Min(-3, message).Validate("")
+	ok = HasMessage(errs, message)
+	if ok {
+		t.Fatal("It should not return an error if min value is negative.")
+	}
+}
+
+func TestValStringMax(t *testing.T) {
+	v := proval.New()
+	message := "any message"
+	errs := v.String("").Max(4, message).Validate("any")
+	ok := HasMessage(errs, message)
+	if ok {
+		t.Fatal("It should not return an error if message length is less than the param.")
+	}
+	errs = v.String("").Max(2, message).Validate("any")
+	ok = HasMessage(errs, message)
+	if !ok {
+		t.Fatal("It should return an error if message length is greater than the param.")
+	}
+	errs = v.String("").Max(-3, message).Validate("")
+	ok = HasMessage(errs, message)
+	if !ok {
+		t.Fatal("It should return an error if min value is negative.")
+	}
+}
+
+func TestValStringEmail(t *testing.T) {
+	v := proval.New()
+	message := "any message"
+	validator := v.String("").Email(message)
+	errs := validator.Validate("any")
+	ok := HasMessage(errs, message)
+	if !ok {
+		t.Fatal("It should return an error if message is not a valid email.")
+	}
+	errs = []error{}
+	errs = append(errs, validator.Validate("any@any.any")...)
+	errs = append(errs, validator.Validate("any.any@any.any")...)
+	fmt.Println(errs)
+	ok = HasMessage(errs, message)
+	if ok {
+		t.Fatal("It should not return an error if message is a valid email.")
 	}
 }
